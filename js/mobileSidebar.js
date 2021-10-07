a47a1244332e25a6b315046acb6e17e37e8512f9
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-10-07 22:47:38 +0100

    responsive sidebar

---
+++ js/mobileSidebar.js
@@ -0,0 +1,8 @@
+      /* Simple appearence with animation AN-1*/
+      function openNav() {
+        document.getElementById('mySidenav').style.width = '280px';
+      }
+      function closeNav() {
+        document.getElementById('mySidenav').style.width = '0';
+      }
+      /* Simple appearence with animation AN-1*/
